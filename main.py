1161d6aa4defa28da2309824af53469e55fb5a4a
Iman Langaran <imanlangaran@gmail.com>
2023-09-05 19:46:13 +0330

    add 'image_detect_to_json' route

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI, File, status
 from fastapi.responses import RedirectResponse
+from PIL import Image
+import io
+import json
+
+from app import get_predict_df
+
 
 app = FastAPI(title="YOLOv8 FastAPI", version="0.0.0")
 
@@ -12,3 +18,15 @@ def home():
 @app.get('/healthcheck', status_code=status.HTTP_200_OK)
 def healthCheck():
     return {'status': 'ok'}
+
+
+@app.post('/image_detect_to_json')
+def image_detect_to_json(file : bytes = File(...)):
+    img = Image.open(io.BytesIO(file)).convert('RGB')
+    
+    preds = get_predict_df(img)
+    
+    result={}
+    result['detected_objects'] = json.loads(preds[['classes', 'confs']].to_json(orient='records'))
+    
+    return result
