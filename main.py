9265dc9a322794db7f1a56f07d9ff7325eec3393
Iman Langaran <imanlangaran@gmail.com>
2023-09-05 19:29:23 +0330

    add healthcheck

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ app = FastAPI(title="YOLOv8 FastAPI", version="0.0.0")
 @app.get('/', include_in_schema=False)
 def home():
     return RedirectResponse(url='/docs')
+
+
+
+@app.get('/healthcheck')
+def healthCheck():
+    return {'status': 'ok'}
