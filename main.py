cdc3b0703ae9064a134fd20c9934760396452398
Iman Langaran <imanlangaran@gmail.com>
2023-09-05 16:54:09 +0330

    add first route

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+app = FastAPI(title="YOLOv8 FastAPI", version="0.0.0")
+
+@app.get('/', include_in_schema=False)
+def home():
+    return RedirectResponse(url='/docs')
