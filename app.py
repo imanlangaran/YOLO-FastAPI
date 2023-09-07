5021455db16c40e0d77250c7534e93c7dabc418d
Iman Langaran <imanlangaran@gmail.com>
2023-09-07 07:38:44 +0330

    added image_segment_to_image route and get_segmented_img function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+
 # from ultralytics.yolo.utils.plotting import Annotator, colors
 from ultralytics.utils.plotting import Annotator, colors
 import pandas as pd
@@ -6,28 +7,37 @@ import numpy as np
 from PIL import Image
 
 
-model = YOLO('models/yolov8n.pt')
-def get_predict_df(file : Image) -> pd.DataFrame:
+model = YOLO("models/yolov8n.pt")
+seg_model = YOLO('models/yolov8n-seg.pt')
+
+
+def get_predict_df(file: Image) -> pd.DataFrame:
     # model = YOLO('models/yolov8n.pt')
 
     result = model.predict(source=file, conf=0.6)
-    bboxes = pd.DataFrame(result[0].to('cpu').numpy().boxes.xyxy, columns=['x1', 'y1', 'x2', 'y2'])
-    bboxes['conf'] = result[0].to('cpu').numpy().boxes.conf
-    bboxes['class'] = (result[0].to('cpu').numpy().boxes.cls).astype(int)
-    bboxes['name'] = bboxes['class'].replace(result[0].names)
-    
+    bboxes = pd.DataFrame(
+        result[0].to("cpu").numpy().boxes.xyxy, columns=["x1", "y1", "x2", "y2"]
+    )
+    bboxes["conf"] = result[0].to("cpu").numpy().boxes.conf
+    bboxes["class"] = (result[0].to("cpu").numpy().boxes.cls).astype(int)
+    bboxes["name"] = bboxes["class"].replace(result[0].names)
+
     return bboxes
 
 
-def draw_boxes(img : Image, preds:pd.DataFrame) -> Image:
+def draw_boxes(img: Image, preds: pd.DataFrame) -> Image:
     annotator = Annotator(np.array(img))
-    preds = preds.sort_values(by=['x1'], ascending=True)
+    preds = preds.sort_values(by=["x1"], ascending=True)
     for i, data in preds.iterrows():
         annotator.box_label(
-            box=[
-                data['x1'],data['y1'],
-                data['x2'],data['y2']
-            ],
-            label=f"{data['name']}: {data['conf']:.2f}", 
-            color=colors(data['class'], bgr=True))
+            box=[data["x1"], data["y1"], data["x2"], data["y2"]],
+            label=f"{data['name']}: {data['conf']:.2f}",
+            color=colors(data["class"], bgr=True),
+        )
     return Image.fromarray(annotator.result())
+
+
+def get_segmented_img(file:Image) -> Image:
+    result = seg_model.predict(source=file, conf=0.6)[0]
+    out_img = Image.fromarray(result.plot()[..., ::-1])
+    return out_img
--- main.py
+++ main.py
@@ -3,41 +3,56 @@ from fastapi.responses import RedirectResponse, StreamingResponse
 from PIL import Image
 import json
 
-from app import get_predict_df, draw_boxes
+from app import get_predict_df, draw_boxes, get_segmented_img
 from helper import Helper
 
 
 app = FastAPI(title="YOLOv8 FastAPI", version="0.0.0")
 
-@app.get('/', include_in_schema=False)
+
+@app.get("/", include_in_schema=False)
 def home():
-    return RedirectResponse(url='/docs')
+    return RedirectResponse(url="/docs")
 
 
-
-@app.get('/healthcheck', status_code=status.HTTP_200_OK)
+@app.get("/healthcheck", status_code=status.HTTP_200_OK)
 def healthCheck():
-    return {'status': 'ok'}
+    return {"status": "ok"}
 
 
-@app.post('/image_detect_to_json')
-def image_detect_to_json(file : bytes = File(...)):
+@app.post("/image_detect_to_json")
+def image_detect_to_json(file: bytes = File(...)):
     img = Helper.get_img_from_bytes(file)
-    
+
     preds = get_predict_df(img)
-    
-    result={}
-    result['detected_objects'] = json.loads(preds[['name', 'conf']].to_json(orient='records'))
-    
+
+    result = {}
+    result["detected_objects"] = json.loads(
+        preds[["name", "conf"]].to_json(orient="records")
+    )
+
     return result
 
 
-@app.post('/image_detect_to_image')
-def image_detect_to_image(file : bytes = File(...)):
+@app.post("/image_detect_to_image")
+def image_detect_to_image(file: bytes = File(...)):
+    img = Helper.get_img_from_bytes(file=file)
+
+    preds = get_predict_df(img)
+
+    out_img = draw_boxes(img=img, preds=preds)
+
+    return StreamingResponse(
+        content=Helper.get_bytes_from_image(img=out_img), media_type="image/jpeg"
+    )
+
+
+@app.post("/image_segment_to_image")
+def image_segment_to_image(file: bytes = File(...)):
     img = Helper.get_img_from_bytes(file=file)
     
-    preds = get_predict_df(img)
+    out_img = get_segmented_img(file=img)
     
-    out_img = draw_boxes(img=img, preds=preds)
-    
-    return StreamingResponse(content=Helper.get_bytes_from_image(img=out_img), media_type="image/jpeg")
+    return StreamingResponse(
+        content=Helper.get_bytes_from_image(img=out_img), media_type="image/jpeg"
+    )
